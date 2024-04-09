8c6ce1b0f8c05d8cc800cb10b068331bad93ba1c
GabMaffei <gabriel.maffei.alm@edu.univali.br>
2024-04-08 21:59:31 -0300

    Hotfix

--- ShaderGraphicsExperiments/Assets/Sumir.cs
+++ ShaderGraphicsExperiments/Assets/Sumir.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Sumir : MonoBehaviour
 {
-    // private Renderer rd;
+    private Renderer rd;
     // Start is called before the first frame update
     void Start()
     {
-        // rd = GetComponent<Renderer>();
+        rd = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //     StartCoroutine(Sumindo());
-        // }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Sumindo());
+        }
     }
 
-    // IEnumerator Sumindo()
-    // {
-    //     for {int i = 0; i< 150; i++}{
-    //         rd.material.SetFloat("_alfa", i * 0.01f);
-    //         yield return new WaitForSeconds(0.1f);
-    //     }
-    // }
+    IEnumerator Sumindo()
+    {
+        for (int i = 0; i< 150; i++){
+            rd.material.SetFloat("_alfa", i * 0.01f);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
