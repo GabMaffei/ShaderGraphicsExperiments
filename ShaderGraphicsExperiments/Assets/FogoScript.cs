f54eee35fc38593d78938ce28a7115900dfcb248
GabMaffei <gabriel.maffei.alm@edu.univali.br>
2024-04-08 21:56:29 -0300

    Final Commit

--- ShaderGraphicsExperiments/Assets/FogoScript.cs
+++ ShaderGraphicsExperiments/Assets/FogoScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class FogoScript : MonoBehaviour
 {
+
+    private Renderer rd;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd = gameObject.GetComponent<Renderer>();
+        rd.material.SetFloat("_alphaC", 0.019f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Camera.main.transform.position);
+        transform.Rotate(90, 0, 0);
     }
 }
---
+++ ShaderGraphicsExperiments/Assets/Sumir.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sumir : MonoBehaviour
+{
+    // private Renderer rd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rd = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     StartCoroutine(Sumindo());
+        // }
+    }
+
+    // IEnumerator Sumindo()
+    // {
+    //     for {int i = 0; i< 150; i++}{
+    //         rd.material.SetFloat("_alfa", i * 0.01f);
+    //         yield return new WaitForSeconds(0.1f);
+    //     }
+    // }
+}
